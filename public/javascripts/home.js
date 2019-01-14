13bb869b5128138eafb1d7473f6022cf9318f370
kanya <qingxingmeixiang@163.com>
2019-01-14 21:24:52 +0800

    add: index data ajax vue

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -1,11 +1,42 @@
 let search = new Vue({
-    el:"#search",
-    data:{
-        searchKeyWord: ""
+    el: "#search",
+    data: {
+        searchKeyWord: "",
+        page:0,
+        line:2,
+        arr: []
     },
-    methods:{
-        search:function () {
-            
+    methods: {
+        search:function(){
+            this.page = 0;
+            this.arr = [];
+            this.$http.post('/home',
+                {
+                    keyWord:this.searchKeyWord,
+                    page:this.page,
+                    line: this.line
+                })
+                .then(function (data) {
+                    console.log(data);
+                    this.arr.push(...data.body);
+                    this.page += this.line;
+                })
+        },
+        nextPage: function () {
+            this.$http.post('/home',
+                {
+                    keyWord:this.searchKeyWord,
+                    page:this.page,
+                    line: this.line
+                })
+                .then(function (data) {
+                console.log(data);
+                this.arr.push(...data.body);
+                this.page += this.line;
+            });
         }
+    },
+    mounted: function () {
+        this.search();
     }
 });
--- routes/home.js
+++ routes/home.js
@@ -1,14 +1,20 @@
 var express = require('express');
 var router = express.Router();
 var db = require('../module/db');
-var infoSql = 'select * from informations';
+let searchSql = "select * from informations where addr like ? limit ?,?";
 
-router.get('/',function (req,resp) {
+router.get('/', function (req, resp) {
+    resp.render('home', {});
+});
 
-   db.query(infoSql,[],function (rows) {
-      console.log(rows);
-      resp.render('home',{info:rows});
-   });
+router.post('/', function (req, resp) {
+    let keyWord = req.body.keyWord;
+    let page = req.body.page;
+    let line = req.body.line;
+    db.query(searchSql, ["%"+keyWord + "%", page,line], function (rows) {
+        console.log("ajax:" + rows);
+        resp.json(rows);
+    });
 });
 
 
